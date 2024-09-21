c4def96a1b2c737b66cdbc53e0f57ec4d2715be2
Ivan Filipenkov <hello@vanutp.dev>
2024-09-21 14:21:30 +0200

    fox commit

--- flake.nix
+++ flake.nix
@@ -9,18 +9,26 @@
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [pkgs.bashInteractive];
         buildInputs = with pkgs; [
           nodePackages.prisma
+          openssl
+          nodejs
+          corepack
+          ffmpeg
         ];
-        shellHook = with pkgs; ''
+        shellHook = ''
           export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
           export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
           export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
           export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+          export CHROME_PATH=${pkgs.google-chrome}/bin/google-chrome-stable
           export PATH="$PWD/node_modules/.bin/:$PATH"
         '';
       };
