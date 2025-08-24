73cc787f2ae9660e1e7bd031d7deb65969348a04
Artem Ivanov <art.ivanov@jetbrains.com>
2025-08-24 12:27:28 +0300

    Fix /api/gif and /api/video

--- src/app/api/video/[id]/route.ts
+++ src/app/api/video/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse, type NextRequest } from "next/server";
 import * as fs from "fs";
 import { getVideoPath } from "~/lib/utils";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
 
 export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
   if (!/^[0-9A-F\-]+$/i.test(params.id)) {
@@ -10,8 +10,8 @@ export async function GET(_: NextRequest, { params }: { params: { id: string } }
   }
   const videoPath = getVideoPath(params.id);
   if (!fs.existsSync(videoPath)) {
-    return new NextResponse(null, { status: 400 });
+    return new NextResponse(null, { status: 404 });
   }
-  const file = fs.readFileSync(videoPath);
+  const file = await fs.openAsBlob(videoPath);
   return new NextResponse(file, { headers: { "content-type": "video/mp4" } });
 }
